eec85f56a951692509e21dfa61fb1c5286dc8611
vagrant <vagrant@controlplane>
2026-09-13 09:48:27 +0000

    컨트롤러 수정

--- springboot-app/hello/src/main/java/com/example/GreetingController.java
+++ springboot-app/hello/src/main/java/com/example/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s ^_______^ V !";
+	private static final String template = "Hello, %s -_______- !!!!!!!!!!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/hello")
